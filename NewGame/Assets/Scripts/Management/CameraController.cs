ed19a6eee9702aa28ffe0fbdaef19d287948f855
Kortis112 <126879970+Kortis112@users.noreply.github.com>
2025-06-04 02:00:19 +0700

    Update 35/50

--- NewGame/Assets/Scripts/Management/CameraController.cs
+++ NewGame/Assets/Scripts/Management/CameraController.cs
@@ -7,6 +7,10 @@ public class CameraController : Singleton<CameraController>
 {
     private CinemachineCamera cinemachineVirtualCamera;
 
+    private void Start()
+    {
+        SetPlayerCameraFollow();
+    }
     public void SetPlayerCameraFollow() {
         cinemachineVirtualCamera = FindFirstObjectByType<CinemachineCamera>();
         cinemachineVirtualCamera.Follow = PlayerController.Instance.transform;
